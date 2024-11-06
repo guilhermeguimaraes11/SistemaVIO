1e09bd0aa0a687afbee9b8f9ad4e82d11dc75113
Guilherme <guilherme10007@gmail.com>
2024-11-06 12:43:21 -0300

    atualizado ate update

---
+++ api/src/controllers/eventoController.js
@@ -0,0 +1,76 @@
+const connect = require("../db/connect");
+
+module.exports = class eventoController {
+  // criação de um evento
+  static async createEvento(req, res) {
+    const { nome, descricao, data_hora, local, fk_id_organizador } = req.body;
+
+    if (!nome || !descricao || !data_hora || !local || !fk_id_organizador) {
+      return res
+        .status(400)
+        .json({ error: "Todos os campos devem ser preenchidos" });
+    }
+    const query = ` INSERT INTO evento (nome,descricao,data_hora,local,fk_id_organizador) VALUES (?,?,?,?,?)`;
+    const values = [nome, descricao, data_hora, local, fk_id_organizador];
+    try {
+      connect.query(query, values, (err) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({ error: "Erro ao criar evento!" });
+        }
+        return res.status(201).json({ message: "Evento criado com sucesso!" });
+      });
+    } catch (error) {
+      console.log("Erro ao executar consulta: ", error);
+      return res.status(500).json({ error: "Erro interno do servido" });
+    }
+  } // fim do 'createEvento'
+
+  static async getAllEventos(req, res) {
+    const query = `SELECT * FROM evento`;
+    try {
+      connect.query(query, (err, results) => {
+        if (err) {
+          console.log(err);
+          return res.status(500).json({ error: "Erro ao buscar eventos" });
+        }
+        return res
+          .status(200)
+          .json({ message: "Eventos listados com sucesso", events: results });
+      });
+    }
+     catch (error) {
+        console.log("Erro ao executar a query:", error);
+        return res.status(500).json({error:"Erro interno do servidor!"})
+     }
+  } // fim getalleventos
+
+  //criaçao do updateeventos
+  static async updateEvento(req, res) {
+    const { id_evento, nome, descricao, data_hora, local, fk_id_organizador } = req.body;
+
+    if (!id_evento ||!nome || !descricao || !data_hora || !local || !fk_id_organizador) {
+      return res
+        .status(400)
+        .json({ error: "Todos os campos devem ser preenchidos" });
+    }
+    const query = ` update evento SET nome = ?, descricao = ?, data_hora = ?, local = ?, fk_id_organizador = ? where id_evento = ?`; 
+    const values = [nome, descricao, data_hora, local, fk_id_organizador, id_evento];
+    try {
+      connect.query(query, values, (err, results) => {
+        console.log("Resultados: ", results);
+        if (err) {
+          console.log(err);
+          return res.status(500).json({ error: "Erro ao atualizar evento!" });
+        }
+        if(results.affectedRows === 0){
+            return res.status(404).json({error:"Evento não encontrado!"});
+        }
+        return res.status(200).json({ message: "Evento atualizado com sucesso!" });
+      });
+    } catch (error) {
+      console.log("Erro ao executar consulta: ", error);
+      return res.status(500).json({ error: "Erro interno do servidor" });
+    }
+  } // fim do 'updateEvento'
+};
--- api/src/routes/apiRoutes.js
+++ api/src/routes/apiRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
-
 const userController = require("../controllers/userController");
 const orgController = require("../controllers/orgController");
+const eventoController = require("../controllers/eventoController");
 
 router.post("/user", userController.createUser);
 router.get("/user", userController.getAllUsers);
@@ -12,5 +12,12 @@ router.post("/org", orgController.createOrg);
 router.get("/org", orgController.getAllOrgs);
 router.put("/org", orgController.updateOrg);
 router.delete("/org/:id_organizador", orgController.deleteOrg);
+
+//rotas eventoController
+router.post("/evento", eventoController.createEvento);
+router.get("/evento", eventoController.getAllEventos);
+router.put("/evento", eventoController.updateEvento);
+
+
 
 module.exports = router;
